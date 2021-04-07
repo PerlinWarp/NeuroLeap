94a3b1fab9a433d061aa15041cf4648bad639d63
perlinwarp <perlinwarp@gmail.com>
2021-04-07 10:50:30 +0100

    Added plotting of simple hand

--- make_dataset.py
+++ make_dataset.py
@@ -14,7 +14,7 @@ NUM_POINTS = 18
 if __name__ == '__main__':
 	leap_arr = multiprocessing.Array('d', range(NUM_POINTS))
 
-	p = multiprocessing.Process(target=d.data_worker, args=(leap_arr, 5, "data_gather5.csv"))
+	p = multiprocessing.Process(target=d.data_worker, args=(leap_arr, 5, "data_gather5abnim.csv"))
 	p.start()
 
 	# Matplotlib Setup
@@ -31,16 +31,47 @@ if __name__ == '__main__':
 			leap_data = leap_data.reshape(3,NUM_POINTS//3)
 			return leap_data
 		return None
+
+	def plot_points(points):
+		patches.set_offsets(points[:2].T)
+		patches.set_3d_properties(points[2], zdir='z')
+
+	def plot_simple(points):
+		'''
+		Plot lines connecting the palms to the fingers, assuming thats the only data we get.
+		'''
+		# Get Palm Position
+		palm = points[:,5]
+		
+		# For Each of the 5 fingers
+		for n in range(0,5):
+			# Draw a line from the palm to the finger tips
+			tip = points[:,n]
+			top = plt3d.art3d.Line3D([palm[0], tip[0]], [palm[1], tip[1]], [palm[2], tip[2]])
+			ax.add_line(top)
 		
 	def animate(i):
 		if (p.is_alive()):
-			points = get_points()
-			if (points is not None):
-				patches.set_offsets(points[:2].T)
-				patches.set_3d_properties(points[2], zdir='z')
+			cpoints = get_points()
 		else:
 			print("Data gatherer has exited")
 			quit()
+
+		# Reset the plot
+		ax.cla()
+		# Really you can just update the lines to avoid this
+		ax.set_xlim3d([-300, 300])
+		ax.set_xlabel('X [mm]')
+		ax.set_ylim3d([-200, 400])
+		ax.set_ylabel('Y [mm]')
+		ax.set_zlim3d([-300, 300])
+		ax.set_zlabel('Z [mm]')
+		
+		if (cpoints is not None):
+			patches = ax.scatter(cpoints[0], cpoints[1], cpoints[2], s=[10]*NUM_POINTS, alpha=1)
+			plot_simple(cpoints)
+			plot_points(cpoints)
+
 
 	anim = animation.FuncAnimation(fig, animate, blit=False, interval=2)
 	plt.show()
